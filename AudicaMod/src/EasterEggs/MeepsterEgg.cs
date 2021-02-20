b8d806264c0edbba017fb8eba92f02868bef7de7
MeepsKitten <meepsalot69@gmail.com>
2021-02-20 11:37:39 -0800

    <3

--- AudicaMod/src/EasterEggs/MeepsterEgg.cs
+++ AudicaMod/src/EasterEggs/MeepsterEgg.cs
@@ -1,4 +1,5 @@
-﻿using MelonLoader;
+﻿using Harmony;
+using MelonLoader;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace AudicaModding.MeepsUIEnhancements.EasterEggs
 {
     public class MeepsterEgg
     {
         public static GameObject Meeps = null;
+        public static GunButton crotch = null;
 
         public static IEnumerator DownloadAsset(string downloadUrl, Action onDownloadComplete = null)
         {
@@ -48,15 +51,38 @@ namespace AudicaModding.MeepsUIEnhancements.EasterEggs
         private static void ActivateAfterDL()
         {
             Meeps.SetActive(true);
-            //Meeps.transform.Rotate(new Vector3(0, 0, 0), Space.World);
+            Meeps.transform.localRotation = Quaternion.Euler(0, 180, 0);
             Meeps.transform.localScale = new Vector3(35,35,35);
             Meeps.transform.localPosition = new Vector3(0, -20, 100);
+            crotch = Meeps.GetComponentInChildren<GunButton>();
+            crotch.doMeshExplosion = false;
+            crotch.doHighlightSound = false;
+            crotch.doParticles = false;
+            crotch.shootSound = null;
+
+            crotch.onHitEvent = new UnityEvent();
+            crotch.onHitEvent.AddListener(new Action(() => { ShotInTheCrotch(); }));
+        }
+
+        private static void ShotInTheCrotch()
+        {
+            Animator anim = Meeps.GetComponent<Animator>();
+            anim.Play("Brutal Assassination");         
         }
 
         public static void HideMeepsterEgg()
         {
             if(Meeps)
                 Meeps.SetActive(false);
+        }
+
+        [HarmonyPatch(typeof(LaunchPanel), "Play")]
+        private static class Noshooty
+        {
+            private static void Postfix(LaunchPanel __instance)
+            {
+                Meeps.GetComponentInChildren<Collider>().enabled = false;
+            }
         }
     }
 }
