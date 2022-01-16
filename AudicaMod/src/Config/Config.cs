93056fe3156d456847c354537f172161f6cd9113
MeepsKitten <meepsalot69@gmail.com>
2022-01-15 16:02:42 -0800

    watch update prep
    config for watch

--- AudicaMod/src/Config/Config.cs
+++ AudicaMod/src/Config/Config.cs
@@ -21,13 +21,20 @@ namespace AudicaModding.MeepsUIEnhancements.Config
 
         public static string SongProgressUISection;
         public static bool SongProgressEnabled;
+        public static bool ShowProgressOnOverlay;
         public static bool ShowProgressOnRight;
         public static bool ShowProgressAsPercentage;
+
+        public static string WatchSettings;
+        public static bool EnableWatch;
+        public static bool SwapToLeftHand;
+        public static bool Use24HourTime;
 
 
         public static string Tweaks;
         public static bool HideOldDifficultyButton;
         public static bool DisableMineSound;
+        public static bool DisableButtonHighlightHaptics;
         public static bool CenterDifficultyButtonText;
         public static bool SongPreviewToggle;
         public static float BloomAmount; //game default is: 5.24
@@ -47,12 +54,19 @@ namespace AudicaModding.MeepsUIEnhancements.Config
 
             MelonPreferences.CreateEntry(CATegory, nameof(SongProgressUISection), "", "[Header]Song Progress Display");
             MelonPreferences.CreateEntry(CATegory, nameof(SongProgressEnabled), true, "Enables a UI element that shows song progress in numerical format above the progress bar");
+            MelonPreferences.CreateEntry(CATegory, nameof(ShowProgressOnOverlay), true, "The progress display will show as an overlay on the desktop window for the game");
             MelonPreferences.CreateEntry(CATegory, nameof(ShowProgressOnRight), false, "Moves the progress UI to the right side of the score display instead of the left");
             MelonPreferences.CreateEntry(CATegory, nameof(ShowProgressAsPercentage), false, "Changes the Song Progress Display to show a percentage of the song completed (as opposed to exact time values)");
+
+           /*MelonPreferences.CreateEntry(CATegory, nameof(WatchSettings), "", "[Header]Watch Settings");
+            MelonPreferences.CreateEntry(CATegory, nameof(EnableWatch), true, "Puts a watch on your right hand");
+            //MelonPreferences.CreateEntry(CATegory, nameof(SwapToLeftHand), false, "Swaps the watch to be on your left hand");
+            MelonPreferences.CreateEntry(CATegory, nameof(Use24HourTime), false, "Time will now be represented using 24 hour time representation");*/
 
 
             MelonPreferences.CreateEntry(CATegory, nameof(Tweaks), "", "[Header]Tweaks");
             MelonPreferences.CreateEntry(CATegory, nameof(DisableMineSound), true, "Disables the sound of mines passing you");
+            //MelonPreferences.CreateEntry(CATegory, nameof(DisableButtonHighlightHaptics), false, "Disables the haptics when highlighting a button");
             MelonPreferences.CreateEntry(CATegory, nameof(HideOldDifficultyButton), false, "If using you're the \"Quick Difficulty Display\" this will hide the old button");
             MelonPreferences.CreateEntry(CATegory, nameof(CenterDifficultyButtonText), true, "Centers the difficulty name text in it's text box");
             MelonPreferences.CreateEntry(CATegory, nameof(SongPreviewToggle), true, "Allows you to shoot the song preview icon to keep the audio playing without needing to continue to hover over it");
@@ -96,11 +110,18 @@ namespace AudicaModding.MeepsUIEnhancements.Config
                         EasterEggs.MeepsterEgg.HideMeepsterEgg();
                     }
 
-                    if ((fieldInfo.Name == nameof(ShowProgressOnRight)))
+                    if (fieldInfo.Name == nameof(ShowProgressOnRight))
                     {
                         var x = MelonPreferences.GetEntryValue<bool>(CATegory, nameof(ShowProgressOnRight)) ? 0.83f : 0.27f;
-                        if(SongTimeUI.localprefab)
-                            SongTimeUI.localprefab.transform.localPosition = new Vector3(x, 1.24f, 1.492f);
+                        if(SongTimeUI.localWorldPrefab)
+                            SongTimeUI.localWorldPrefab.transform.localPosition = new Vector3(x, 1.24f, 1.492f);
+                    }
+
+                    if(fieldInfo.Name == nameof(SwapToLeftHand))
+                    {
+                        var leftHand = MelonPreferences.GetEntryValue<bool>(CATegory, nameof(SwapToLeftHand));
+                        if (!WatchController.watch) return;
+                        //WatchController.watch.transform.localRotation = Quaternion.EulerRotation(leftHand ? new Vector3(0, 180, 0) : Vector3.zero);
                     }
 
                 }
